85d03b4ea2f050c01b4755607b26dfb173ea0a80
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-16 07:47:37 -1000

    Proper init
    Initial commit for csharp survival guide

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string myName = "jonathan";
+    public int myAge = 26;
+    public string myLocation = "Atlanta";
+    public bool haskey = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myName = "loren";            
+        Debug.Log("My name is: " + myName);
+        Debug.Log(myAge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
