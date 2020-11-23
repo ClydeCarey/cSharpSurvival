c84fce21a3dc21dd7ee4fdb5246367b9ec34feac
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-22 22:27:49 -1000

    CR02
    challenge review

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-
-    public string myName = "jonathan";
-    public int myAge = 26;
-    public string myLocation = "Atlanta";
-    public bool haskey = false;
-
+    //create a variable to save name
+    //variable age
+    //speed
+    //health
+    //score
+    //collected all keys
+    //ammo
+    public string playerName = "Duke Nukem";
+    public int age = 30;
+    public float speed = 22.1f;
+    public float health = 85.5f;
+    public int myScore = 39;
+    public bool hasAllKeys = false;
+    public int ammo = 40;
+        
     // Start is called before the first frame update
     void Start()
     {
-        myName = "loren";            
-        Debug.Log("My name is: " + myName);
-        Debug.Log(myAge);
+        Debug.Log("My name is: " + playerName);
+        Debug.Log("I am " + age + "years old");
+        Debug.Log("My speed is: " + speed);
+        Debug.Log("I have " + health + "HP");
+        Debug.Log("My score is: " + myScore);
+        Debug.Log("I have all the keys is: " + hasAllKeys);
+        Debug.Log("I have " + ammo + "bullets");
+
     }
 
     // Update is called once per frame
