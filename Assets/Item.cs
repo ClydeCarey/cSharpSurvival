40ccc93c2931fa13192341f6a50764194873c6e0
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-22 22:56:33 -1000

    tipcalc
    ablit to start tipcalculator

---
+++ Assets/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //name
+    //description
+    //image
+    //attack strength
+
+    public string itemName = "Sword";
+    public string itemDescription = "A stout one handed sword";
+    public Sprite _swordSprite;
+    public float attackStrength = 32.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Item name" + itemName);
+        
+    }
+
+}
