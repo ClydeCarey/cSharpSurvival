0706d4318710d5f68eadf8cb7b0549a0c5b814c7
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-23 06:55:27 -1000

    calc
    finished

---
+++ Assets/TipCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour
+{
+    //bill is 40 dollars
+    //tip is 20% or user amount
+    //calculate total
+    float bill = 40.0f;
+    public float tip = 20f;
+    float total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //your bill is, your tip is, so you owe: totalamount
+        total = (bill * (tip/100)) + bill;
+        Debug.Log("Your bill is " + bill + " Your tip is " + tip + "% Your total is " + total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
