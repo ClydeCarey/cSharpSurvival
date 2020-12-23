379236cde87f707691dfed685c1a60ca848e0cf8
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-12-22 16:30:37 -1000

    else-if
    else if

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,32 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //create a variable to save name
-    //variable age
-    //speed
-    //health
-    //score
-    //collected all keys
-    //ammo
-    public string playerName = "Duke Nukem";
-    public int age = 30;
-    public float speed = 22.1f;
-    public float health = 85.5f;
-    public int myScore = 39;
-    public bool hasAllKeys = false;
-    public int ammo = 40;
-        
-    // Start is called before the first frame update
+    public int easyDifficulty = 0;
+    public int mediumDifficulty = 1;
+    public int hardDifficulty = 2;
+    public int currentDifficulty = 1;
+
+    
     void Start()
     {
-        Debug.Log("My name is: " + playerName);
-        Debug.Log("I am " + age + "years old");
-        Debug.Log("My speed is: " + speed);
-        Debug.Log("I have " + health + "HP");
-        Debug.Log("My score is: " + myScore);
-        Debug.Log("I have all the keys is: " + hasAllKeys);
-        Debug.Log("I have " + ammo + "bullets");
-
+        currentDifficulty = mediumDifficulty;
     }
 
     // Update is called once per frame
@@ -37,4 +20,9 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    //if score >= 50
+    
+
+    //then print "you are awesome"
 }
---
+++ Assets/QuizCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCalculator : MonoBehaviour
+{
+    public float quiz1;
+    public float quiz2;
+    public float quiz3;
+    public float quiz4;
+    public float quiz5;
+    //create random quiz values for 5 quiz grades and print average
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quiz1 = Random.Range(0.0f, 100.0f);
+        quiz2 = Random.Range(0.0f, 100.0f);
+        quiz3 = Random.Range(0.0f, 100.0f);
+        quiz4 = Random.Range(0.0f, 100.0f);
+        quiz5 = Random.Range(0.0f, 100.0f);
+
+        Debug.Log("Grade 1: " + quiz1 + "Grade 2: " + quiz2 + "Grade 3: " + quiz3 + "Grade 4: " + quiz4 + "Grade 5: " + quiz5);
+        float averageScore = ((quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5);
+        averageScore = Mathf.Round(averageScore * 100.0f) / 100.0f;
+
+        Debug.Log("the average score is: " + averageScore );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
